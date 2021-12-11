89bbe77e93c3f48663ecbffb870fb567a8a64cf2
Neelam  Kumari <neelamKumari@gmail.com>
2021-12-11 21:02:13 +0530

    day2 second commit

---
+++ lib/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+class HomePage extends StatelessWidget {
+  final int days=30;
+   final String name= "Mahi";
+  // const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+        body: Center(
+          child: Container(
+            child: Text("welcome to $days of flutter by &Mahi"),
+          ),
+        ),
+        drawer: Drawer(),
+    );
+      
+    
+  }
+}
