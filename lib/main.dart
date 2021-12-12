0b34bbb6ca91e16320c6cde8888b492f973332da
Neelam  Kumari <neelamKumari@gmail.com>
2021-12-12 11:06:44 +0530

    day3 third commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/home_page.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
 
 void main() {
   runApp(NeelamApp());
@@ -15,7 +16,22 @@ class NeelamApp extends StatelessWidget {
   Widget build(BuildContext context) {
     
     return MaterialApp(
-      home: HomePage(),
+      
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+
+      ),
+      routes: {
+
+     
+      "/":(context)=>LoginPage(),
+      "/home":(context)=>HomePage(),
+      "/Login":(context)=>LoginPage()
+      
+       },
+
     );
   }
 }
