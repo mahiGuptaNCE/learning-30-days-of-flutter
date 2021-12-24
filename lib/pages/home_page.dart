14335130fe284777148c79b6905c755737fbf17f
Neelam  Kumari <neelamKumari@gmail.com>
2021-12-24 20:44:50 +0530

    day12 commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
+
 import 'package:flutter_catalog/widget/drawer.dart';
+import 'package:flutter_catalog/widget/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   final int days=30;
@@ -8,15 +11,23 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
+  final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text("Catalog App")),
+        
+        title: Text("Catalog App"),
+      // ignore: dead_code
       ),
-        body: Center(
-          child: Container(
-            child: Text("welcome to $days of flutter by &Mahi"),
-          ),
+        body: Padding(
+          padding:const EdgeInsets.all(16.0),
+          child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index){
+            return ItemWidget(
+               item: dummyList[index],
+            );
+          }
+        )
         ),
         drawer: MyDrawer(),
     );
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,7 @@
 // import 'dart:convert';
 
 import 'package:flutter/material.dart';
+
 import 'package:flutter_catalog/utils/routes.dart';
 
 // ignore: must_be_immutable
