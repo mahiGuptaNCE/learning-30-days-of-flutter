8b4558df45075b5df75ca7d47dcc855202aa719f
Neelam  Kumari <neelamKumari@gmail.com>
2021-12-26 10:18:45 +0530

    day13 commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_catalog/models/catalog.dart';
-
 import 'package:flutter_catalog/widget/drawer.dart';
 import 'package:flutter_catalog/widget/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days=30;
+
    final String name= "Mahi";
-  // const HomePage({ Key? key }) : super(key: key);
 
   @override
+  void initState(){
+    super.initState();
+    loadData();
+  }
+  loadData()async{
+  final catalogjson= await rootBundle.loadString("assets/files/catalog.json");
+  final decodedData = jsonDecode(catalogjson);
+ var productsData =decodedData["products"];
+ print(productsData);
+  }
   Widget build(BuildContext context) {
   final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
