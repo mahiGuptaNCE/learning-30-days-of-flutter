ba2c8b78b6400aa65b65c7a7ce580e714f680e58
Neelam  Kumari <neelamKumari@gmail.com>
2022-01-01 09:32:30 +0530

    day15 commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -21,13 +21,18 @@ class _HomePageState extends State<HomePage> {
     loadData();
   }
   loadData()async{
+    await Future.delayed(Duration(seconds: 2));
   final catalogjson= await rootBundle.loadString("assets/files/catalog.json");
   final decodedData = jsonDecode(catalogjson);
  var productsData =decodedData["products"];
- print(productsData);
+ CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
+  
   }
   Widget build(BuildContext context) {
-  final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
+  // final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         
@@ -36,18 +41,19 @@ class _HomePageState extends State<HomePage> {
       ),
         body: Padding(
           padding:const EdgeInsets.all(16.0),
-          child: ListView.builder(
-          itemCount: dummyList.length,
-          itemBuilder: (context, index){
-            return ItemWidget(
-               item: dummyList[index],
-            );
-          }
-        )
-        ),
-        drawer: MyDrawer(),
+         // ignore: unnecessary_null_comparison
+         child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+            ? ListView.builder(
+                itemCount: CatalogModel.items.length,
+                itemBuilder: (context, index) => ItemWidget(
+                  item: CatalogModel.items[index],
+                ),
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
+      ),
+      drawer: MyDrawer(),
     );
-      
-    
   }
 }
